fc113ee7a1052f7eb3ea004ec78a2f6b143535b6
Trollguyjr <agonzalez3411@tpaa.org>
2024-01-13 16:39:50 -0800

    OpenCV import for Cam

--- Test/src/main/java/frc/robot/RobotContainer.java
+++ Test/src/main/java/frc/robot/RobotContainer.java
@@ -22,6 +22,8 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
+  private final  OpenC CV = new OpenC();
+
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
